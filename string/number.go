d62690345906a98a4f61c71de6d9a294e9a1823d
Artjoms Nemiro <LinMAD@users.noreply.github.com>
2024-02-08 12:29:17 +0100

    Implement new string package to cast whole number easier (#33)
    * Implement new string package to work with whole number - gracefully cast with overflow handling
    
    * Simplify code by introducing private methods for number parsing, and add benchmark.

---
+++ string/number.go
@@ -0,0 +1,77 @@
+package string
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+// WholeNumber type.
+type WholeNumber interface {
+	uint | uint8 | uint16 | uint32 | uint64 | int | int8 | int16 | int32 | int64
+}
+
+// ToWholeNumber safely converting string to given whole number.md with positive or negative handling.
+func ToWholeNumber[T WholeNumber](str string) (T, error) {
+	const errTmpl string = "failed to convert string to WholeNumber, error: %v"
+	var result T
+	isWithErr := true
+
+	maxBitSize, maxBitSizeErr := defineMaxIntType(T(0))
+	if maxBitSizeErr != nil {
+		return 0, fmt.Errorf(errTmpl, "unable define bit size for number parsing")
+	}
+
+	result = parseInt[T](&str, &maxBitSize, &isWithErr)
+	if result == 0 && isWithErr {
+		result = parseUint[T](&str, &maxBitSize, &isWithErr)
+	}
+
+	if isWithErr {
+		return 0, fmt.Errorf(errTmpl, "string value not matching any whole number premitive type")
+	}
+
+	return result, nil
+}
+
+func parseInt[T WholeNumber](str *string, maxBitSize *int, isWithErr *bool) (result T) {
+	isNegativeInt := strings.HasPrefix(*str, "-")
+	maxIntStr := strconv.FormatInt(math.MaxInt64, 10)
+	if isNegativeInt || len(*str) <= len(maxIntStr) {
+		if pInt, pIntErr := strconv.ParseInt(*str, 10, *maxBitSize); pIntErr == nil {
+			result = T(pInt)
+			*isWithErr = false
+		}
+	}
+	return result
+}
+
+func parseUint[T WholeNumber](str *string, maxBitSize *int, isWithErr *bool) (result T) {
+	maxUintStr := strconv.FormatUint(math.MaxUint64, 10)
+	if len(*str) <= len(maxUintStr) {
+		if pUint, pUintErr := strconv.ParseUint(*str, 10, *maxBitSize); pUintErr == nil {
+			result = T(pUint)
+			*isWithErr = false
+		}
+	}
+	return result
+}
+
+func defineMaxIntType(number any) (maxBitSize int, err error) {
+	switch reflect.TypeOf(number).Kind() {
+	case reflect.Int8, reflect.Uint8:
+		maxBitSize = 8
+	case reflect.Int16, reflect.Uint16:
+		maxBitSize = 16
+	case reflect.Int, reflect.Int32, reflect.Uint, reflect.Uint32:
+		maxBitSize = 32
+	case reflect.Int64, reflect.Uint64:
+		maxBitSize = 64
+	default:
+		err = fmt.Errorf("unable to define type of (%v) it's not part of WholeNumber", number)
+	}
+
+	return
+}
---
+++ string/number_test.go
@@ -0,0 +1,125 @@
+package string
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type wholeNumberTestCase struct {
+	name      string
+	input     string
+	want      interface{}
+	wantError bool
+}
+
+// TestToWholeNumber tests function with various inputs that converts string to WholeNumber.
+func TestToWholeNumberInt(t *testing.T) {
+	// Define test cases
+	testCases := []wholeNumberTestCase{
+		// Test cases with Int
+		{"PositiveInt8", "100", int8(100), false},
+		{"PositiveInt16", "32767", int16(32767), false},
+		{"NegativeInt32", "-67890", int32(-67890), false},
+		{"MaxInt64", "9223372036854775807", int64(9223372036854775807), false},
+		{"MinInt64", "-9223372036854775808", int64(-9223372036854775808), false},
+		// Test cases for Uint
+		{"MaxByteAliasToUint8", "5", byte(5), false},
+		{"MaxUint8", "255", uint8(255), false},
+		{"MaxUint16", "65535", uint16(65535), false},
+		{"MaxUint32", "4294967295", uint32(4294967295), false},
+		{"MaxUint64", "18446744073709551615", uint64(18446744073709551615), false},
+
+		// General test cases
+		{"BeyondMaxUint64", "18446744073709551616", nil, true},
+		{"Zero", "0", int64(0), false},
+		{"NegativeZero", "-0", int64(0), false},
+		{"SmallNegativeInt", "-1", int64(-1), false},
+		{"InvalidNumber", "not_a_number", nil, true},
+		{"Hexadecimal", "0x1F", nil, true},
+		{"PartialInvalid", "123abc", nil, true},
+		{"LargeUint", "18446744073709551615", uint64(18446744073709551615), false},
+		{"EmptyString", "", nil, true},
+		{"PositiveOverflowInt64", "19223372036854775807", nil, true},
+		{"PositiveOverflowUint64", "28446744073709551615", nil, true},
+		{"NegativeOverflowInt64", "-10223372036854775809", nil, true},
+		{"FloatNumber", "42.2", nil, true},
+	}
+
+	// Verify int
+	t.Run(testCases[0].name, func(t *testing.T) { runTestCase[int8](testCases[0], t) })
+	t.Run(testCases[1].name, func(t *testing.T) { runTestCase[int16](testCases[1], t) })
+	t.Run(testCases[2].name, func(t *testing.T) { runTestCase[int32](testCases[2], t) })
+	t.Run(testCases[3].name, func(t *testing.T) { runTestCase[int64](testCases[4], t) })
+	t.Run(testCases[4].name, func(t *testing.T) { runTestCase[int64](testCases[4], t) })
+
+	// Verify uint
+	t.Run(testCases[5].name, func(t *testing.T) { runTestCase[byte](testCases[5], t) })
+	t.Run(testCases[6].name, func(t *testing.T) { runTestCase[uint8](testCases[6], t) })
+	t.Run(testCases[7].name, func(t *testing.T) { runTestCase[uint16](testCases[7], t) })
+	t.Run(testCases[8].name, func(t *testing.T) { runTestCase[uint32](testCases[8], t) })
+	t.Run(testCases[9].name, func(t *testing.T) { runTestCase[uint64](testCases[9], t) })
+
+	// Verify general test cases
+	t.Run(testCases[10].name, func(t *testing.T) { runTestCase[uint64](testCases[10], t) })
+	t.Run(testCases[11].name, func(t *testing.T) { runTestCase[int64](testCases[11], t) })
+	t.Run(testCases[12].name, func(t *testing.T) { runTestCase[int64](testCases[12], t) })
+	t.Run(testCases[13].name, func(t *testing.T) { runTestCase[int64](testCases[13], t) })
+	t.Run(testCases[14].name, func(t *testing.T) { runTestCase[uint64](testCases[14], t) })
+	t.Run(testCases[15].name, func(t *testing.T) { runTestCase[uint64](testCases[15], t) })
+	t.Run(testCases[16].name, func(t *testing.T) { runTestCase[uint64](testCases[16], t) })
+	t.Run(testCases[17].name, func(t *testing.T) { runTestCase[uint64](testCases[17], t) })
+	t.Run(testCases[18].name, func(t *testing.T) { runTestCase[uint64](testCases[18], t) })
+	t.Run(testCases[19].name, func(t *testing.T) { runTestCase[int64](testCases[19], t) })
+	t.Run(testCases[20].name, func(t *testing.T) { runTestCase[uint64](testCases[20], t) })
+	t.Run(testCases[21].name, func(t *testing.T) { runTestCase[int64](testCases[21], t) })
+	t.Run(testCases[22].name, func(t *testing.T) { runTestCase[int](testCases[22], t) })
+}
+
+func runTestCase[T WholeNumber](tc wholeNumberTestCase, t *testing.T) {
+	got, err := ToWholeNumber[T](tc.input)
+
+	// Check for error expectation
+	if tc.wantError {
+		assert.NotNil(t, err, "expected an error but got none")
+	} else if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if !tc.wantError {
+		// Check if the result is what we expect
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("expected %v, got %v", tc.want, got)
+		}
+	}
+}
+
+func BenchmarkProcessData(b *testing.B) {
+	testCases := []struct {
+		name  string
+		input string
+	}{
+		{"ShortString", "123"},
+		{"MediumString", "12345"},
+		{"LongString", "1234567890"},
+		{"NegativeNumber", "-12345"},
+		{"LeadingZeros", "000012345"},
+		{"MaxInt", "2147483647"},
+		{"BeyondMaxInt", "2147483648"},
+		{"NegativeInt", "-552183648"},
+		{"LargeUint64", "18446744073709551615"},
+		{"Zero", "0"},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := ToWholeNumber[uint64](tc.input)
+				if err != nil {
+					b.Fatal("Benchmark failed with error:", err)
+				}
+			}
+		})
+	}
+}
