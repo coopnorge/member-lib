52a53145fc3811529f2c04462596fe33a133d87a
Artjoms Nemiro <artjoms.nemiro@coop.no>
2024-05-22 10:47:33 +0200

    Update contextext to store values and ContextExtended inside context.Context

--- contextext/contextext_test.go
+++ contextext/contextext_test.go
@@ -3,11 +3,14 @@ package contextext
 import (
 	"context"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
+
+type unitStubContextKey struct{}
 
 func TestContextExtended(t *testing.T) {
 	baseCtx := context.Background()
@@ -52,8 +55,8 @@ func TestContextExtended(t *testing.T) {
 func TestSafelyExtractExtendedContextFromInterface(t *testing.T) {
 	t.Run("CorrectType", func(t *testing.T) {
 		expectedValue := "test value"
-		extCtx := &ContextExtended[string]{ctx: context.Background(), values: make(map[string]string)}
-		extCtx.values["unit"] = expectedValue
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue("unit", expectedValue)
 
 		result, err := SafelyExtractExtendedContextFromInterface[string](extCtx)
 		if err != nil {
@@ -64,7 +67,8 @@ func TestSafelyExtractExtendedContextFromInterface(t *testing.T) {
 	})
 
 	t.Run("IncorrectType", func(t *testing.T) {
-		extCtx := &ContextExtended[int]{ctx: context.Background(), values: make(map[string]int)}
+		extCtx := NewContextExtended[int](context.Background())
+
 		_, err := SafelyExtractExtendedContextFromInterface[string](extCtx)
 		if err == nil {
 			t.Fatalf("expected an error, got nil")
@@ -89,5 +93,130 @@ func TestSafelyExtractExtendedContextFromInterface(t *testing.T) {
 		if err.Error() != expectedError {
 			t.Fatalf("expected error message %v, got %v", expectedError, err.Error())
 		}
+	})
+
+	t.Run("VanillaContext", func(t *testing.T) {
+		expectedValue := "test value"
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue("unit", expectedValue)
+
+		vanillaCtx, vanillaCtxCancel := context.WithDeadline(extCtx, time.Now())
+		vanillaCtxCancel()
+
+		result, err := SafelyExtractExtendedContextFromInterface[string](vanillaCtx)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		assert.Equal(t, extCtx.values, result.values)
+	})
+
+	t.Run("duplicatedKeyOfContextExt", func(t *testing.T) {
+		expectedValue := "test value"
+
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue("unit", expectedValue)
+
+		extCtx2 := NewContextExtended[string](context.Background())
+
+		extCtx2Result, extCtx2ResultErr := SafelyExtractExtendedContextFromInterface[string](extCtx2)
+		if extCtx2ResultErr != nil {
+			t.Fatalf("expected no error, got %v", extCtx2ResultErr)
+		}
+
+		_, not := extCtx2Result.GetValue("unit")
+		assert.False(t, not, "expected to be not found value")
+
+		extCtxResult, extCtxResultErr := SafelyExtractExtendedContextFromInterface[string](extCtx)
+		if extCtxResultErr != nil {
+			t.Fatalf("expected no error, got %v", extCtxResultErr)
+		}
+
+		resultValue, in := extCtxResult.GetValue("unit")
+		assert.True(t, in, "expected to be not found value")
+		assert.True(t, resultValue == expectedValue)
+	})
+
+	t.Run("duplicatedKeyOfContextExtWithVanillaContext", func(t *testing.T) {
+		expectedValue := "test value"
+
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue("unit", expectedValue)
+
+		extCtx2 := NewContextExtended[string](context.Background())
+
+		extCtx2Result, extCtx2ResultErr := SafelyExtractExtendedContextFromInterface[string](extCtx2)
+		if extCtx2ResultErr != nil {
+			t.Fatalf("expected no error, got %v", extCtx2ResultErr)
+		}
+
+		_, not := extCtx2Result.GetValue("unit")
+		assert.False(t, not, "expected to be not found value")
+
+		vanillaCtx, vanillaCtxCancel := context.WithDeadline(extCtx, time.Now())
+		vanillaCtxCancel()
+
+		extCtxResult, extCtxResultErr := SafelyExtractExtendedContextFromInterface[string](vanillaCtx)
+		if extCtxResultErr != nil {
+			t.Fatalf("expected no error, got %v", extCtxResultErr)
+		}
+
+		resultValue, in := extCtxResult.GetValue("unit")
+		assert.True(t, in, "expected to be not found value")
+		assert.True(t, resultValue == expectedValue)
+	})
+
+	t.Run("ContextExtended as int", func(t *testing.T) {
+		expectedValue := 42
+		extCtx := NewContextExtended[int](context.Background())
+		extCtx.AddValue("unit", expectedValue)
+
+		result, err := SafelyExtractExtendedContextFromInterface[int](extCtx)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		assert.Equal(t, extCtx.values, result.values)
+	})
+
+	t.Run("ContextExtended as struct", func(t *testing.T) {
+		expectedValue := "42"
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue(unitStubContextKey{}, expectedValue)
+
+		result, err := SafelyExtractExtendedContextFromInterface[string](extCtx)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		resultValue, _ := result.GetValue(unitStubContextKey{})
+		assert.True(t, resultValue == expectedValue)
+		assert.Equal(t, extCtx.values, result.values)
+	})
+
+	t.Run("ContextExtended as ContextExtended", func(t *testing.T) {
+		extCtxValue := "42"
+		extCtx := NewContextExtended[string](context.Background())
+		extCtx.AddValue(unitStubContextKey{}, extCtxValue)
+
+		extCtxOfCtxValue := "universe"
+		extCtxOfCtx := NewContextExtended[string](extCtx)
+		extCtxOfCtx.AddValue("answer", extCtxOfCtxValue)
+
+		extCtxResult, extCtxErr := SafelyExtractExtendedContextFromInterface[string](extCtx)
+		if extCtxErr != nil {
+			t.Fatalf("expected no error, got %v", extCtxErr)
+		}
+
+		extCtxResultValue, _ := extCtxResult.GetValue(unitStubContextKey{})
+		assert.True(t, extCtxResultValue == extCtxValue)
+
+		extCtxOfCtxResult, extCtxOfCtxErr := SafelyExtractExtendedContextFromInterface[string](extCtxOfCtx)
+		if extCtxOfCtxErr != nil {
+			t.Fatalf("expected no error, got %v", extCtxOfCtxErr)
+		}
+
+		extCtxOfCtxResultValue, _ := extCtxOfCtxResult.GetValue("answer")
+		assert.True(t, extCtxOfCtxResultValue == extCtxOfCtxValue)
 	})
 }
