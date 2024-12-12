e9d2ce3b3d3cca263c925f36c2df3f5f3ef43fc1
Hadrien Kohl <hadrien.kohl@gmail.com>
2024-12-12 22:11:35 +0100

    Improve documentation

--- configloader/configloader.go
+++ configloader/configloader.go
@@ -74,6 +74,15 @@ func (m MissingEnvErr) Error() string {
 	return fmt.Sprintf("missing variable '%s' for the field '%s'", m.Key, m.Field.String())
 }
 
+// WithTypeHandler registers a custom type conversion function for a specific type T.
+// The function should convert a string environment value to type T, returning an error
+// if the conversion fails.
+//
+// Example:
+//
+//	configloader.Load(&cfg, WithTypeHandler(func(s string) (time.Duration, error) {
+//	    return time.ParseDuration(s)
+//	}))
 func WithTypeHandler[T any](f func(string) (T, error)) Option {
 	return func(l *Loader) {
 		var zero T
@@ -83,34 +92,76 @@ func WithTypeHandler[T any](f func(string) (T, error)) Option {
 	}
 }
 
-// WithNameTag sets the tag used to override environment variable names.
+// WithNameTag sets the struct tag that can be used to override the environment variable name
+// for a field. If a field has this tag, its value will be used as the exact environment variable
+// name, ignoring any prefix and name conversion.
+//
+// Example:
+//
+//	type Config struct {
+//	    Host string `env:"SERVICE_HOST"` // Will look for SERVICE_HOST environment variable
+//	}
+//	configloader.Load(&cfg, WithNameTag("env"))
 func WithNameTag(tag string) Option {
 	return func(l *Loader) {
 		l.nameTag = tag
 	}
 }
 
-// WithDefaultTag sets the tag to use for default values.
+// WithDefaultTag sets the struct tag used for specifying default values.
+// If an environment variable is not found, the value of this tag will be used instead.
+//
+// Example:
+//
+//	type Config struct {
+//	    Port int `default:"8080"`  // Will use 8080 if PORT is not set
+//	}
+//	configloader.Load(&cfg, WithDefaultTag("default"))
 func WithDefaultTag(tag string) Option {
 	return func(l *Loader) {
 		l.defaultTag = tag
 	}
 }
 
-// WithPrefix sets the prefix for environment variable names.
+// WithPrefix sets a prefix that will be prepended to all environment variable names.
+// The prefix and field name will be joined with an underscore.
+//
+// Example:
+//
+//	type Config struct {
+//	    Port int  // Will look for "APP_PORT" environment variable
+//	}
+//	configloader.Load(&cfg, WithPrefix("APP"))
 func WithPrefix(prefix string) Option {
 	return func(l *Loader) {
 		l.prefix = prefix
 	}
 }
 
-// WithEnv sets a custom environment variable lookup function.
+// WithEnv sets a custom function for looking up environment variables.
+// This is primarily useful for testing or when environment variables need to be
+// sourced from somewhere other than os.LookupEnv.
+//
+// The function should return the value and a boolean indicating whether the variable
+// was found, similar to os.LookupEnv.
 func WithEnv(env func(string) (string, bool)) Option {
 	return func(l *Loader) {
 		l.env = env
 	}
 }
 
+// Load takes a pointer to a struct and populates its fields from environment variables.
+// Field names are converted to SCREAMING_SNAKE_CASE by default to form environment variable names.
+// For example, a field named "DatabaseURL" would look for an environment variable named "DATABASE_URL".
+//
+// The supported field types are determined by the registered type handlers. Use WithTypeHandler
+// to add support for custom types.
+//
+// Returns an error if:
+// - The value is not a pointer to a struct
+// - Required environment variables are missing
+// - Type conversion fails for any field
+// - Any field has an unsupported type
 func Load(value any, opts ...Option) error {
 	loader := defaultLoader()
 	for _, opt := range opts {
