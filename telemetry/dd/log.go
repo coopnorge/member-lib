97a65445a1335279d8e6efe5cd7ac92a74c52c8a
Kirk Easterson <kirk.easterson@gmail.com>
2026-08-24 12:02:53 +0200

    Fix otel issues with bumping version

--- telemetry/dd/log.go
+++ telemetry/dd/log.go
@@ -5,16 +5,14 @@ import (
 	"context"
 	"strconv"
 
-	"go.opentelemetry.io/otel/trace"
-
-	"go.opentelemetry.io/otel/log"
+	"go.opentelemetry.io/otel/attribute"
 	sdklog "go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var _ sdklog.Processor = &ddProcessor{}
 
-type ddProcessor struct {
-}
+type ddProcessor struct{}
 
 func (p *ddProcessor) Shutdown(_ context.Context) error {
 	return nil
@@ -24,12 +22,14 @@ func (p *ddProcessor) ForceFlush(_ context.Context) error {
 	return nil
 }
 
+func (p *ddProcessor) Enabled(ctx context.Context, _ sdklog.EnabledParameters) bool { return true }
+
 func (p *ddProcessor) OnEmit(ctx context.Context, record *sdklog.Record) error {
 	if span := trace.SpanFromContext(ctx); span.IsRecording() {
 		osCtx := ctxWrapper{span.SpanContext()}
 		record.AddAttributes(
-			log.String("dd.span_id", strconv.FormatUint(osCtx.SpanID(), 10)),
-			log.String("dd.trace_id", strconv.FormatUint(osCtx.TraceID(), 10)),
+			attribute.String("dd.span_id", strconv.FormatUint(osCtx.SpanID(), 10)),
+			attribute.String("dd.trace_id", strconv.FormatUint(osCtx.TraceID(), 10)),
 		)
 	}
 	return nil
